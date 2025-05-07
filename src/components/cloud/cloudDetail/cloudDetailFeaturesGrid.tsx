36cd27444a89d50dab17df4fdbef7387d520193a
Nghị <nghingothanhnghi@gmail.com>
2025-05-07 21:30:37 +0700

    update

--- src/components/cloud/cloudDetail/cloudDetailFeaturesGrid.tsx
+++ src/components/cloud/cloudDetail/cloudDetailFeaturesGrid.tsx
@@ -20,17 +20,21 @@ const CloudDetailFeaturesGrid: React.FC<CloudDetailFeaturesGridProps> = ({ setup
             ref={containerRef}
             className="w-full h-full"
         >
-            <div className='flex relative align-top'>
-                <div className='flex align-top'>
-                    <div className='relative w-266'>
+            <div className='flex flex-row md:flex relative align-top mt-8'>
+                <div className='flex align-top max-md:flex-col relative'>
+                    <div className='relative w-[266px] md:w-[150px] sm:w-[150px]'>
                         <Circle type="type1" className='' />
                     </div>
-                    <div className='relative w-310 left-[-39px] top-[-119px]'>
+                    <div className='relative w-[310px] md:w-[178px] sm:w-[200px] left-[-39px] top-[-119px] md:left-[-20px] md:top-[-57px]'>
                         <Circle type="type2" className='' />
                     </div>
-                    <div className='relative w-269 left-[-74px] top-[-6px]'>
+                    <div className='relative w-[269px] md:w-[154px] sm:w-[180px] left-[-74px] top-[-6px] md:left-[-38px] md:top-[-3px]'>
                         <Circle type="type3" className='' />
                     </div>
+                </div>
+                <div className='relative mt-4'>
+                    {/* This div is now below the type1, type2, and type3 blocks */}
+                    <p>Content below type1, type2, and type3 blocks</p>
                 </div>
             </div>
         </div>
