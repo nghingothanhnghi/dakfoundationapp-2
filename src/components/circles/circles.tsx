9196628252b8037870841f2d2b9f4018d1b05175
Nghị <nghingothanhnghi@gmail.com>
2025-05-11 20:17:03 +0700

    update

--- src/components/circles/circles.tsx
+++ src/components/circles/circles.tsx
@@ -4,9 +4,22 @@ interface CircleProps {
   type: 'type1' | 'type2' | 'type3'; // Define the types of circles you have
   fill?: string;
   className?: string; // Add className prop
+  icon?: React.ReactNode; // Add icon prop
 }
 
-const Circle: React.FC<CircleProps> = ({ type, fill = 'white', className }) => {
+const Circle: React.FC<CircleProps> = ({ type, fill = 'white', className, icon }) => {
+  const renderIcon = () => {
+    if (icon) {
+      return (
+        <foreignObject x="0" y="0" width="100%" height="100%">
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100%' }}>
+          {icon}
+        </div>
+      </foreignObject>
+      );
+    }
+    return null;
+  };
   switch (type) {
     case 'type1':
       return (
@@ -18,6 +31,7 @@ const Circle: React.FC<CircleProps> = ({ type, fill = 'white', className }) => {
           className={className} // Apply className
         >
           <path d="M109.788 266.103L0.724609 223.194C44.304 120.363 123.832 43.1437 218.771 0.466744L265.519 107.917C198.07 138.607 141.466 193.375 109.788 266.103Z" fill={fill} />
+          {renderIcon()}
         </svg>
       );
     case 'type2':
@@ -30,6 +44,7 @@ const Circle: React.FC<CircleProps> = ({ type, fill = 'white', className }) => {
           className={className} // Apply className
         >
           <path d="M47.5805 139.943L0.816149 32.4864C96.2434 -6.3981 205.784 -11.022 309.556 27.3285L266.65 136.387C192.99 109.826 115.487 112.977 47.5805 139.943Z" fill={fill} />
+          {renderIcon()}
         </svg>
       );
     case 'type3':
@@ -42,6 +57,7 @@ const Circle: React.FC<CircleProps> = ({ type, fill = 'white', className }) => {
           className={className} // Apply className
         >
           <path d="M161.547 262.96C129.826 196.111 74.2496 140.432 0.890838 109.914L43.7844 0.888275C147.224 43.2796 225.252 121.85 268.978 216.168L161.547 262.96Z" fill={fill} />
+          {renderIcon()}
         </svg>
       );
     default:
--- src/components/cloud/cloudDetail/cloudDetailFeaturesGrid.tsx
+++ src/components/cloud/cloudDetail/cloudDetailFeaturesGrid.tsx
@@ -1,6 +1,10 @@
 import React, { useEffect, useRef } from 'react';
 import Circle from '../../circles/circles';
 import CloudLogoIcon from '../../../assets/products/cloud_icon.png';
+import SmeIcon from '../../../assets/icons/businesses_icon.png';
+import PersonalIcon from '../../../assets/icons/personal_icon.png';
+import GovertmentIcon from '../../../assets/icons/dak_government_icon.png';
+
 
 
 interface CloudDetailFeaturesGridProps {
@@ -25,17 +29,30 @@ const CloudDetailFeaturesGrid: React.FC<CloudDetailFeaturesGridProps> = ({ setup
                 <div className='md:w:[65%] flex flex-col md:flex relative align-top mt-8'>
                     <div className='flex align-top max-md:flex-col relative'>
                         <div className='relative w-[266px] md:w-[150px] sm:w-[150px]'>
-                            <Circle type="type1" className='' />
+                            <Circle 
+                            type="type1" 
+                            className='' 
+                            icon={<img src={SmeIcon} alt="icon" style={{ width: '100', height: '100px' }} />}
+                             />
+                            
                         </div>
                         <div className='relative w-[310px] md:w-[178px] sm:w-[200px] left-[-39px] top-[-119px] md:left-[-20px] md:top-[-57px]'>
-                            <Circle type="type2" className='' />
+                            <Circle 
+                            type="type2" 
+                            className=''
+                            icon={<img src={PersonalIcon} alt="icon" style={{ width: '100', height: '100px' }} />}
+                            />
                         </div>
                         <div className='relative w-[269px] md:w-[154px] sm:w-[180px] left-[-74px] top-[-6px] md:left-[-38px] md:top-[-3px]'>
-                            <Circle type="type3" className='' />
+                            <Circle 
+                            type="type3" 
+                            className=''
+                            icon={<img src={GovertmentIcon} alt="icon" style={{ width: '100', height: '100px' }} />}
+                            />
                         </div>
                     </div>
                     <div className='relative md:left-[-23px] md:top-[-119px]'>
-                        <img className='object-contain w-full md:aspect-[1.70]' src={CloudLogoIcon}/> 
+                        <img className='object-contain w-full md:aspect-[1.70]' src={CloudLogoIcon} alt="Cloud Logo" /> 
                     </div>
                 </div>
             </div>
